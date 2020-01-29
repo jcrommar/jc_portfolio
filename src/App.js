880ff90a0c0c126e3e7f2f82ea03bbfa3e0450c5
Juan C. Romualdo <jromualdo@razimports.com>
2020-01-29 14:38:02 -0600

    my app changes for JC

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       resumeData: {}
     };
 
-    ReactGA.initialize('UA-110570651-1');
+    ReactGA.initialize('');
     ReactGA.pageview(window.location.pathname);
 
   }
