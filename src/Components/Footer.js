7bb3c69cfc88cc71645df5069dccc38d16bbccc9
Juan C. Romualdo <jromualdo@razimports.com>
2020-01-30 08:45:14 -0600

    updated copyright information

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 J.C.R.M</li>
-              <li>Design by <a title="Styleshout" href="http://www.github.com/jcrommar">ME</a></li>
+              <li>Redesigned by <a title="Styleshout" href="http://www.github.com/jcrommar">ME</a></li>
            </ul>
 
         </div>
